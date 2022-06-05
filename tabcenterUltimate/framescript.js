98254ce2096a623d343662df55b8f8fcbde51875
Alex Mehler <alexmehler@t-online.de>
2022-06-05 23:37:34 +0200

    Add files via upload

--- tabcenterUltimate/framescript.js
+++ tabcenterUltimate/framescript.js
@@ -17,37 +17,19 @@ function yyy(){
 }
 
 function xxx(){
-
-	console.log("jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj");
 	
-
-	// if (timer == true) return;
-	// timer = true;
-	// setTimeout(function(){
-		
-		// timer = false;
-		
-	// },1000);
 	let a;
 	let b;
 	let snapshotCanvas = content.document.createElement('canvas');
 	let finalCanvas = content.document.createElement('canvas');
 	
-	//snapshotCanvas.mozOpaque = true;
-	//snapshotCanvas.imageSmoothingEnabled = false;
-
-	//finalCanvas.mozOpaque = true;
-	//finalCanvas.imageSmoothingEnabled = false;
-	
-	
-	
-	console.log("Grab Screenshot");
-	if(content.document.contentType.toLowerCase().indexOf('image/') == 0){
+	if(content.document.contentType.toLowerCase().indexOf('image/') == 0){ // if just an image is loaded in the tab, not a website
 		image = content.document.getElementsByTagName('img')[0];
+		
 		//----------------CAPTURE IMAGE
-		console.log("ISIMAGE");
+		//console.log("ISIMAGE");
 		
-		
+		//convert to either 54px width or 36px height depending on image size
 		if(image.width > image.height){
 			//ctx.drawImage(image, 0, 0, 54,54*image.height/image.width);
 			a = 54;
@@ -60,17 +42,11 @@ function xxx(){
 		let faktor = 2;
 		snapshotCanvas.width = 54*faktor;
 		snapshotCanvas.height = 36*faktor;
-		
-		let scale = Math.min(Math.max(a*2 / content.innerWidth,
-                                  b*2 / content.innerHeight), 1);
 
 		let snapshotCtx = snapshotCanvas.getContext("2d");
-		//snapshotCtx.save();
-		//snapshotCtx.scale(scale, scale);
 		snapshotCtx.drawImage(image, 0, 0, a*faktor, b*faktor);
-		//snapshotCtx.restore();
 		
-		//----- PART2 DOWNSCALE---------------
+		//----- PART2 DOWNSCALE to final size 54x36--------------- we need to downscale 2 times in a row , otherwise the result is super pixelated
 		
 		finalCanvas.width = 54;
 		finalCanvas.height = 36;
@@ -85,7 +61,7 @@ function xxx(){
 		finalCtx.restore();
 		
 		
-	}else{
+	}else{  // ---------capture normal website
 	
 		//----------------CAPTURE WINDOW
 		a = 54;
@@ -102,7 +78,7 @@ function xxx(){
 							   snapshotCtx.DRAWWINDOW_DO_NOT_FLUSH);
 		snapshotCtx.restore();
 		
-		//----- PART2 DOWNSCALE---------------
+		//----- PART2 DOWNSCALE to final size 54x36--------------- we need to downscale 2 times in a row , otherwise the result is super pixelated
 		
 		finalCanvas.width = a;
 		finalCanvas.height = b;
@@ -115,21 +91,20 @@ function xxx(){
 		finalCtx.drawImage(snapshotCanvas, 0, 0);
 		finalCtx.restore();
 	}
-	
-	
-	
-	
+
+
+	// -------------------------------- finally lets convert to a base64 dataURL and send it back to the uc.js script
 	
 	let finalImage = finalCanvas.toDataURL();
 	//console.log(finalImage);
-	if (finalImage == 	"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADYAAAAkCAYAAADCW8lNAAAARElEQVRYhe3PAQ0AIAzAsPs3DSoeMtIq2OZ8al4HbDFWY6zGWI2xGmM1xmqM1RirMVZjrMZYjbEaYzXGaozVGKsxVnMBeARDY2yTgBUAAAAASUVORK5CYII="){
-		console.log("WEI?ESSSSS BIIIILLLLLLLDDDDDD!!!!! &&&&&&&&&&&");
-		return;
-	};
-	if (finalImage == 	"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADYAAAAkCAYAAADCW8lNAAAAHklEQVRYhe3BMQEAAADCoPVPbQlPoAAAAAAAAADgYx6EAAEp8hhdAAAAAElFTkSuQmCC"){
-		console.log("LÄÄÄÄÄÄÄÄÄÄRRRES BIIIILLLLLLLDDDDDD!!!!! &&&&&&&&&&&");
-		return;
-	};
+	// if (finalImage == 	"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADYAAAAkCAYAAADCW8lNAAAARElEQVRYhe3PAQ0AIAzAsPs3DSoeMtIq2OZ8al4HbDFWY6zGWI2xGmM1xmqM1RirMVZjrMZYjbEaYzXGaozVGKsxVnMBeARDY2yTgBUAAAAASUVORK5CYII="){
+	// 	console.log("WEI?ESSSSS BIIIILLLLLLLDDDDDD!!!!! &&&&&&&&&&&");
+	// 	return;
+	// };
+	// if (finalImage == 	"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADYAAAAkCAYAAADCW8lNAAAAHklEQVRYhe3BMQEAAADCoPVPbQlPoAAAAAAAAADgYx6EAAEp8hhdAAAAAElFTkSuQmCC"){
+	// 	console.log("LÄÄÄÄÄÄÄÄÄÄRRRES BIIIILLLLLLLDDDDDD!!!!! &&&&&&&&&&&");
+	// 	return;
+	// };
 	
 	sendAsyncMessage("tabCenterUltimate@Ulf3000:my-e10s-extension-message", finalImage);	
 }
